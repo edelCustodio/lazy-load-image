07b6e4cb8e40b2aaacba38691f38e29483a6d64f
edelCustodio <edelcustodiofrias@gmail.com>
2021-01-13 22:20:25 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+import LazyImage from "./LazyImage/LazyImage";
+
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-align: center;
+  }
+`;
+
+const Grid = styled.div`
+  display: grid;
+  padding: 16px;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Global />
+      <h1>Random Images</h1>
+      <Grid>
+        {[ ...Array(100).keys() ].map(i => (
+          <LazyImage
+            key={i}
+            src={`https://picsum.photos/1000/1000?random=${i}`}
+            alt={`Random image ${i}`}
+          />
+        ))}
+      </Grid>
     </div>
   );
 }
